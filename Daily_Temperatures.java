098a749151cf61ed9c46eb3cc57311e4ab1b877a
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-09-14 23:24:19 -0500

    Update Daily_Temperatures.java

--- Daily_Temperatures.java
+++ Daily_Temperatures.java
@@ -26,7 +26,7 @@ class Daily_Temperatures_BruteForce {
 
 /**************************Using Stack******************************/
 //Time Complexity : O(2n)
-//Space Complexity : O(1)
+//Space Complexity : O(n)
 //Did this code successfully run on Leetcode : Yes
 //Any problem you faced while coding this : No
 
