db742fad8e77d590c93d03ca045805874cf5af85
anudeepareddy <40484243+anudeepareddy@users.noreply.github.com>
2020-10-27 12:40:49 -0500

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,56 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//Daily Temperatures
+// Time Complexity :0(n+n)
+// Space Complexity :0(n) for stack
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        if(T.length==0 || T==null)
+            return new int[0];
+        Stack<Integer> stack= new Stack<>();
+        int[] output=new int[T.length];
+        for(int i=0;i<T.length;i++)
+        {
+            while(!stack.isEmpty() && T[stack.peek()]<T[i])
+            {
+                int top=stack.pop();
+                output[top]=i-top;
+            }
+            stack.push(i);
+        }
+        return output;
+    }
+}
+
+//Next Greater Element II
+// Time Complexity :0(n+n)
+// Space Complexity :0(n) for stack
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        if(nums==null || nums.length==0)
+            return new int[0];
+        int[] output= new int[nums.length];
+        Arrays.fill(output,-1);
+        Stack<Integer> stack= new Stack<>();
+        for(int x=0;x<nums.length*2;x++)
+        {
+            int i= x%nums.length;
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i])
+            {
+                int top=stack.pop();
+                output[top]=nums[i];
+            }
+            if(output[i]==-1)
+                stack.push(i);
+        }
+        return output;
+    }
+}
