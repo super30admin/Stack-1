96fe09dcbaa80de4c98efe5009262d3e4b91e823
Jeevtesh Singh <50637812+jeevy222@users.noreply.github.com>
2020-11-23 21:30:57 -0500

    Create nge2.py

---
+++ nge2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        size = len(nums)
+        nums+=nums
+        res = [-1] * size
+        stack = []
+        for i in list(range(size))*2:
+            while stack and (nums[stack[-1]] < nums[i]):
+                res[stack.pop()] = nums[i]
+            stack.append(i)
+        return res
+        
+        
+        #tc and sc = o(n) 
