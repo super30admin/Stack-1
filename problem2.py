e6b1f575a3546cc58848f643e4f4110b2513a145
Akash Tanwani <77640435+akashtanwani@users.noreply.github.com>
2021-04-29 00:07:30 -0500

    completed Problem2

---
+++ problem2.py
@@ -0,0 +1,22 @@
+#Time Complexity=O(n)
+#Space Complexity=O(1)
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        if not nums or len(nums)==0:
+            return []
+        n=len(nums)
+        output=[-1 for i in range(n)]
+        stack=deque()
+        for i in range(2*n):
+            current=nums[i%n]
+            while stack and current>nums[stack[-1]]:
+                index=stack.pop()
+                output[index]=current
+            if output[i%n]== -1:
+                stack.append(i%n)
+        
+        return output
+            
+        
+        
