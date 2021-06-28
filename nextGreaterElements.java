7e16992e31899e008670fb5d880fd1df8e7b383a
Shrinivas Miskin <64209820+Shrinivasmiskin@users.noreply.github.com>
2021-06-28 19:54:22 +0530

    next Greater Elements using stacks
    Time Complexity = O(4n) == O(n)
    Space Complexity = O(n)

---
+++ nextGreaterElements.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int [] result = new int [n];
+        if(nums == null || nums.length == 0) return result;
+        Stack<Integer> st = new Stack<>();
+        Arrays.fill(result, -1);
+        
+        //iterate the array twice
+        for(int i = 0; i < 2*n; i++){
+            //if stack not empty & incoming element is greater than top of stack
+            while((!st.isEmpty()) && nums[i % n] > nums[st.peek()]){
+                int index = st.pop();
+                result[index] = nums[i % n];
+            }
+            //push the elements in stack only once
+            if(i < n){
+                st.push(i % n);
+            }
+        }
+        return result;
+    }
+}
