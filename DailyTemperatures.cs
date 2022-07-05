10b1e1d09619338b01df48ac81a96947e186b94e
Rincy Soman <rincysoman@gmail.com>
2022-07-04 23:28:35 -0700

    Daily Temperature

---
+++ DailyTemperatures.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    /// Time Complexity : O(N) 
+    // Space Complexity :O(N) 
+    // Did this code successfully run on Leetcode :Yes
+    // Any problem you faced while coding this :  No 
+    public class DailyTemperatures
+    {
+        public int[] DailyTemperaturesProb(int[] temperatures)
+        {
+            int[] result = new int[temperatures.Length];
+            if (temperatures == null || temperatures.Length == 0) return result;
+            Stack<int> st = new Stack<int>();
+            st.Push(0);
+            for (int i = 1; i < temperatures.Length; i++)
+            {
+                while (st.Count != 0 && temperatures[i] > temperatures[st.Peek()])
+                {
+                    int curr = st.Pop();
+                    result[curr] = i - curr;
+                }
+                st.Push(i);
+            }
+            return result;
+
+        }
+    }
+}
