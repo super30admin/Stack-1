96edfad2582b25d53afc9b094ec3257bc4ad35d2
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-02 15:08:13 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,26 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//****130.739. Daily Tempratures- BRUTEFORCE WAY****
+// Time Complexity :O(N square);
+// Space Complexity :o(1);
+// Did this code successfully run on Leetcode :Y
+// Any problem you faced while coding this :N;
 
-
-// Your code here along with comments explaining your approach
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n=temperatures.length;
+        int [] answer=new int[n];
+        
+        for(int i=0;i<temperatures.length;i++)
+        {
+            for(int j=i+1;j<temperatures.length;j++)
+            {
+                if(temperatures[i]<temperatures[j])
+                {
+                    answer[i]=j-i;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
