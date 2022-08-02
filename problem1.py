44a74fa0ed7686b4f9ac9b4b067ca0bdd114b63f
Vineet Khanna <67041692+vineetvk19@users.noreply.github.com>
2022-08-02 03:36:40 -0400

    Solved DailyTemperatures problem

---
+++ problem1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stk = []
+        n = len(temperatures)
+        result = [0] * n
+        for i in range(n):
+            while len(stk) != 0 and temperatures[i] > temperatures[stk[-1]]:
+                temp = stk.pop()
+                result[temp] = i - temp
+            stk.append(i)
+        return result
