e6d7dce7c2ece6780b08187fff19e3a72055a881
poojitha bijjam <poojitha1589@gmail.com>
2022-08-01 20:59:24 -0500

    Create nextGreater.py

---
+++ nextGreater.py
@@ -0,0 +1,16 @@
+#Time complexity: O(n)
+#Space complexity: O(n)
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [-1 for _ in range(n)]
+        st = []
+        
+        for i in range(2*n):
+            while st and nums[i%n] > nums[st[-1]]:
+                res[st[-1]] = nums[i%n]
+                st.pop()
+            if i < n:
+                st.append(i)
+        return res
+            
