1f23ad221e53bb12e22cac6cc894b2d486283b0c
Riggy0508 <rigved.2017@gmail.com>
2022-12-20 12:52:23 -0800

    Completed-Stack-1 Python

---
+++ daily temperature.py
@@ -0,0 +1,16 @@
+# TC: O(n+m)
+# SC: O(n)
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        res=[0]*len(temperatures)
+
+        stack=[] #In this stack we are gonna keep a pair of [temp,index]
+
+        for i,t in enumerate(temperatures):
+            while stack and t>stack[-1][0]:
+                temp,index=stack.pop()
+                res[index]=(i-index)
+            stack.append([t,i])
+        return res
