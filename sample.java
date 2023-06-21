ece9ac3029a2b8a3771ec9ffd69869b0a581bbe5
Pranay Joshi <pranay@Pranays-MBP.attlocal.net>
2023-06-21 01:04:12 -0500

    Completed Assignment_42 on Stack-1 Please Review

--- sample.java
+++ sample.java
@@ -1,7 +1,53 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+## Problem1 Daily Temperatures (https://leetcode.com/problems/daily-temperatures/)
+
+// Time Complexity : 0(n)
+// Space Complexity : 0(n)
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        if (temperatures == null || temperatures.length == 0) {
+            return new int[] {};
+        }
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && temperatures[i] > temperatures[s.peek()]) {
+                int popped = s.pop();
+                result[popped] = i - popped;
+            }
+            s.push(i);
+        }
+        return result;
+    }
+}
 
 
-// Your code here along with comments explaining your approach
+## Problem2 Next Greater Element II (https://leetcode.com/problems/next-greater-element-ii/)
+
+// Time Complexity : 0(n)
+// Space Complexity : 0(n)
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+        Stack<Integer> s = new Stack<>();
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+
+        for (int i = 0; i < n * 2; i++) {
+            while (!s.isEmpty() && nums[i % n] > nums[s.peek()]) {
+                int popped = s.pop();
+                result[popped] = nums[i % n];
+            }
+            if (i < n) {
+                s.push(i);
+            }
+        }
+        return result;        
+    }
+}
