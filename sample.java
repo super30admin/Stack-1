e939c8f53d919834445540fb4594f4c272b0a19d
Pratyush Srivastava <pratyshsrivastava96@gmail.com>
2023-09-06 12:37:34 -0700

    daily tempreture and next greater element added

--- sample.java
+++ sample.java
@@ -5,3 +5,57 @@
 
 
 // Your code here along with comments explaining your approach
+//Daily Tempreture
+Time : o(2n) -> o(n) as we are making 2 passes one over array and one over stack in worst ClassCastException
+Space -> o(n) -> Space of stack
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> st = new Stack();
+        for(int i=0;i<temperatures.length;i++){
+            while(!st.isEmpty() && temperatures[st.peek()]<temperatures[i]){
+                int popped = st.pop();
+               result[popped] = i - popped;
+
+            }
+            st.push(i);
+        }
+
+        return result;
+        
+    }
+}
+
+//Next greater element
+//Time ->o(2n)->o(n) as we are making 2 pass
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        Stack<Integer> st = new Stack();
+        int[] result = new int[n];
+        Arrays.fill(result,-1);
+        for(int i=0;i<2*n;i++){
+            if(i>n){
+                 if(i%n==st.peek()){
+                break;
+            }
+            }
+            
+             while(!st.isEmpty() && nums[st.peek()]<nums[i%n]){
+                 int popped = st.pop();
+                 result[popped] = nums[i%n];
+
+             }
+             if(i<n){
+                 st.push(i);
+             }
+        }
+
+        return result;
+        
+    }
+}
+
+
