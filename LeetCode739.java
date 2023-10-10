98df2193b3f8c3929e7f180f8182201f49f8cb0a
Sri Manaswi Chirumamilla <srimanaswichirumamilla@Sris-MacBook-Air.local>
2023-10-09 21:59:24 -0400

    Stack-1 completed

---
+++ LeetCode739.java
@@ -0,0 +1,24 @@
+
+// Time Complexity : O(n) 
+// Space Complexity : O(n) for stack
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        if(temperatures == null || temperatures.length==0) return new int[0];
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<temperatures.length;i++){
+            // store all small temperatures in stack and process them when greater temperature comes
+            while(!stack.isEmpty()&& temperatures[stack.peek()]<temperatures[i]){   
+                int popped = stack.pop();
+                result[popped]= i-popped;    
+            }
+            stack.push(i);    // push the current temperature
+        }
+        return result;
+    }
+}
