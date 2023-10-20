ae2a8477bcf8d5703be53abdf02c5fde5febe917
Gopi Vaka <vakagopi80@gmail.com>
2023-10-20 17:48:38 -0500

    completed Stack-1

---
+++ DailyTemperature.java
@@ -0,0 +1,20 @@
+// SC = O(n)
+// Tc = O(n)
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        if(temperatures == null && temperatures.length == 0) return new int[0];
+        int[] result = new int[temperatures.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i < temperatures.length ; i++){
+            if(i == 0) st.push(i);
+            while(!st.isEmpty() && temperatures[st.peek()] < temperatures[i]){
+                int popped = st.pop();
+                result[popped] = i - popped;
+            }
+            st.push(i);
+        }
+        return result;
+        
+    }
+}
