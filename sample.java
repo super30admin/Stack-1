27abdcb16dacc8a908e33570ca5c0217e3ac774a
Apeksha Kotian <apekshakotian@Apekshas-MacBook-Pro.local>
2023-12-05 18:00:36 -0600

    Completed Stack-1

--- sample.java
+++ sample.java
@@ -1,7 +1,54 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// # Stack-1
+
+// ## Problem1 Daily Temperatures (https://leetcode.com/problems/daily-temperatures/)
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :I didnt face any problem while coding this
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int[] result = new int[temperatures.length];
+        for(int i=0; i<temperatures.length; i++){
+            while(!st.empty() && temperatures[i]>temperatures[st.peek()]){
+            int popped = st.pop();
+            result[popped] = i - popped;
+        }st.push(i);
+        }
+        return result; 
+    }
+}
+
+// ## Problem2 Next Greater Element II (https://leetcode.com/problems/next-greater-element-ii/)
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :I didnt face any problem while coding this
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] result = new int[nums.length];
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        Arrays.fill(result, -1);
+        for(int i=0; i< 2 * n; i++){
+            while(!st.isEmpty() && nums[i%n] > nums[st.peek()]){
+                int popped = st.pop();
+                result[popped] = nums[i%n];
+            }
+            if(i < n){
+                st.push(i);
+            }
+        }
+        return result;
+    }
+}
