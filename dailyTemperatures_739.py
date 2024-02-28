090bc0d1c58aede7074072e07a68439cd19a63dd
Nitesh Yaduvanshi <niteshkumar@dal.ca>
2024-02-28 14:04:28 -0400

    Adding comments

--- dailyTemperatures_739.py
+++ dailyTemperatures_739.py
@@ -1,6 +1,9 @@
 #Monotonic decreasing stack
 #Time complexity = O(n)
 #Space complexity = O(n)
+#Did this code successfully run on Leetcode: Yes
+#Any problem you faced while coding this : No
+
 class Solution(object):
     def dailyTemperatures(self, temperatures):
 
@@ -11,8 +14,10 @@ class Solution(object):
         for r in range(len(temperatures)):
             #while loop to compare current and previous temperatue.
             while stack and temperatures[stack[-1]] < temperatures[r]:
+
                 l = stack.pop() #pop the top element (colder temperature)
-                answer[l] = r - l
+
+                answer[l] = r - l  #Difference of the indexes of current & previous colder temperatures
 
             stack.append(r)
         
